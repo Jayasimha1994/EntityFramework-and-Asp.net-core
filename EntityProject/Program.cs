e40841464cc416607d88cb59218b163121dca858
jayasimha <jayasimhajay1994@gmail.com>
2018-03-22 20:10:24 +0530

    EntityFramework project

---
+++ EntityProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new con1Entities1();
+            Person person = new Person()
+            {
+                p_id = 14,
+                p_name = "jayasimha"
+            };
+            context.People.Add(person);
+            context.SaveChanges();
+        }
+    }
+}
